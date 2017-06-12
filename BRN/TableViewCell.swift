dfcd87fef6d9fcb7e6cf92d8df0a3e6dec654ee0
student <student@Achievers-iMac-3.local>
2017-06-12 18:32:36 +0530

    TableView

---
+++ BRN/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  BRN
+//
+//  Created by student on 6/12/17.
+//  Copyright © 2017 student. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
